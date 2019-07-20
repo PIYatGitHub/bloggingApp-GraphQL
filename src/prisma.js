042de905ad04dedd74add0460e332565fda1d7dd
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2019-07-20 22:41:22 +0300

    code now reads from the db and logs a query to the screen!

--- src/prisma.js
+++ src/prisma.js
@@ -2,5 +2,10 @@ import {Prisma} from 'prisma-binding'
 
 const prisma = new Prisma({
   typeDefs: 'src/generated/prisma.graphql',
-  endpoint: 'localhost:4466'
+  endpoint: 'http://localhost:4466'
 });
+
+prisma.query.users(null, '{ id name email createdAt updatedAt posts {id  title}}')
+    .then((data)=>{
+      console.log(JSON.stringify(data, undefined, 4));
+    });

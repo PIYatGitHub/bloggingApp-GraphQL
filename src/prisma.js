d7d047f7791555db79574ff8d932bc67409789c2
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2019-07-20 23:02:08 +0300

    messing with some mutations

--- src/prisma.js
+++ src/prisma.js
@@ -5,7 +5,36 @@ const prisma = new Prisma({
   endpoint: 'http://localhost:4466'
 });
 
-prisma.query.users(null, '{ id name email createdAt updatedAt posts {id  title}}')
-    .then((data)=>{
-      console.log(JSON.stringify(data, undefined, 4));
-    });
+// prisma.mutation.createPost({
+//   data: {
+//     title: "POST I MOLITVI",
+//     body: "lets see",
+//     published: true,
+//     author: {
+//       connect: {
+//         id: "cjy2wu4e600nc0986pudngywm"
+//       }
+//     }
+//   }
+// }, '{id title body published author {id name}}')
+//   .then((data) => {
+//     console.log(JSON.stringify(data, undefined, 4));
+//     return prisma.query.users(null, '{id name email}')
+//   }).then((data) => {
+//   console.log(JSON.stringify(data, undefined, 4));
+// });
+
+
+prisma.mutation.updatePost({
+  data:{title:"Alternative Realities exist...", published: false},
+  where:{
+    id:"cjy2wuxr400o2098654aiv6pu"
+  }
+}, '{id title body published}')
+  .then((data) => {
+    console.log(JSON.stringify(data, undefined, 4));
+    return prisma.query.posts(null, '{id title body published}')
+  }).then((data) => {
+  console.log(JSON.stringify(data, undefined, 4));
+});
+

0bdc8ecb924e22f6e5be252c30cd145673bf231e
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2019-07-21 11:39:57 +0300

    CRUD for posts is now done -- remaining: comments

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -4,13 +4,15 @@ const Mutation = {
   async createUser(parent, args, {prisma}, info){
     const emailTaken = await prisma.exists.User({email:args.data.email});
     if (emailTaken) throw new Error('Email already taken');
-    return prisma.mutation.createUser({ data: args.data }, info);
+    return prisma.mutation.createUser({data: args.data},
+      info);
   },
   async updateUser(parent, args, {prisma}, info){
     const userExists = await prisma.exists.User({id:args.id});
     if (!userExists) throw new Error('No user found');
     return prisma.mutation.updateUser({
-      where: {id: args.id}, data: args.data
+      where: {id: args.id},
+      data: args.data
     }, info);
   },
   async deleteUser(parent, args, {prisma}, info){
@@ -27,17 +29,24 @@ const Mutation = {
         body: args.data.body,
         published: args.data.published,
         author:{
-          connect: {
-            id:args.data.author
-          }
+          connect: {id:args.data.author}
         }
       }}, info);
   },
-  async updatePost(parent, args, {prisma, pubsub}, info){
-
+  async updatePost(parent, args, {prisma}, info){
+    const postExists = await prisma.exists.Post({id:args.id});
+    if (!postExists) throw new Error('No such post');
+    return prisma.mutation.updatePost({
+      where: {id: args.id},
+      data:args.data
+    }, info);
   },
-  async deletePost(parent, args, {prisma, pubsub}, info){
-
+  async deletePost(parent, args, {prisma}, info){
+    const postExists = await prisma.exists.Post({id:args.id});
+    if (!postExists) throw new Error('No such post');
+    return prisma.mutation.deletePost({
+      where: {id: args.id}
+    }, info);
   },
   async createComment(parent, args, {prisma, pubsub}, info){
 

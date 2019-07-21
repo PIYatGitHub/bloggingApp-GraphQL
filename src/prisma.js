b983bb3377879e3327595f211cf638403dd27d9c
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2019-07-21 08:37:36 +0300

    now done with async and await

--- src/prisma.js
+++ src/prisma.js
@@ -5,6 +5,59 @@ const prisma = new Prisma({
   endpoint: 'http://localhost:4466'
 });
 
+// const createPostForUser = async (authorID, data)=> {
+//   const post = await prisma.mutation.createPost({
+//     data:{...data,
+//     author:{
+//       connect:{
+//           id:authorID
+//         }
+//       }
+//     }
+//   }, '{ id }');
+//
+//   const user = await prisma.query.user({
+//       where: {
+//         id:authorID
+//       }
+//   }, '{ id name email posts {id title published} }');
+//
+//   return user
+// };
+//
+// createPostForUser('cjy2wu4e600nc0986pudngywm',{
+//     title: "Great reads",
+//     body: "Idiot - Leo Tolstoy",
+//     published: true
+// }).then((user)=>{
+//   console.log(JSON.stringify(user, undefined, 2));
+// });
+
+
+const updatePostForUser = async (postID, data)=> {
+  const post = await prisma.mutation.updatePost({
+    where:{
+      id:postID
+    }, data
+  }, '{ author {id} }');
+
+  const user = await prisma.query.user({
+      where: {
+        id:post.author.id
+      }
+  }, '{ email posts {id title body published} }');
+
+  return user
+};
+
+updatePostForUser('cjy2wuxr400o2098654aiv6pu',{
+    body: "THIS IS A FAKE UPDATE...",
+    published: true
+}).then((user)=>{
+  console.log(JSON.stringify(user, undefined, 2));
+});
+
+
 // prisma.mutation.createPost({
 //   data: {
 //     title: "POST I MOLITVI",
@@ -24,17 +77,18 @@ const prisma = new Prisma({
 //   console.log(JSON.stringify(data, undefined, 4));
 // });
 
+//
+// prisma.mutation.updatePost({
+//   data:{title:"Alternative Realities exist...", published: false},
+//   where:{
+//     id:"cjy2wuxr400o2098654aiv6pu"
+//   }
+// }, '{id title body published}')
+//   .then((data) => {
+//     console.log(JSON.stringify(data, undefined, 4));
+//     return prisma.query.posts(null, '{id title body published}')
+//   }).then((data) => {
+//   console.log(JSON.stringify(data, undefined, 4));
+// });
 
-prisma.mutation.updatePost({
-  data:{title:"Alternative Realities exist...", published: false},
-  where:{
-    id:"cjy2wuxr400o2098654aiv6pu"
-  }
-}, '{id title body published}')
-  .then((data) => {
-    console.log(JSON.stringify(data, undefined, 4));
-    return prisma.query.posts(null, '{id title body published}')
-  }).then((data) => {
-  console.log(JSON.stringify(data, undefined, 4));
-});
 

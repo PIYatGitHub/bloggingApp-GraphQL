e8150d0554ffe55a964e3c21b7611d048ae4fe9c
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2019-07-21 12:43:41 +0300

    subscriptions and pubications for comments and posts are in place

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -1,15 +1,16 @@
 const Subscription = {
   comment: {
-    subscribe(parent, {postID}, {db, pubsub}, info){
-      const post = db.posts_seed.find((p)=> p.id === postID && p.published)
-      if (!post) throw new Error('Post not found');
-
-      return pubsub.asyncIterator(`comment ${postID}`)
+    subscribe(parent, {postID}, {prisma}, info){
+      return prisma.subscription.comment({
+        where: {node: { post: {id:postID}}}
+      }, info);
     }
   },
   post: {
-    subscribe(parent, args, {pubsub}, info){
-      return pubsub.asyncIterator('post')
+    subscribe(parent, args, {prisma}, info){
+      return prisma.subscription.post({
+        where: {node: {published:true} }
+      }, info);
     }
   }
 };

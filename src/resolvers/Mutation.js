5aece74e51abd79a71924cfcfacd2e0f7fd7a993
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2019-07-22 17:50:45 +0300

    update and delete user are now locked down!

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -9,48 +9,45 @@ const Mutation = {
     if (args.data.password.length < 8) throw new Error('Password must be at least 8 characters long');
     let password = await bcrypt.hash(args.data.password, 10);
     const user = prisma.mutation.createUser({
-        data: {
-          ...args.data,
-          password
-        }
-      });
-
+      data: {
+        ...args.data,
+        password
+      }
+    });
     return {
       user,
-      token: jwt.sign({userID:user.id}, 'ThisIsULTRAsecreT')
+      token: jwt.sign({userID: user.id}, 'ThisIsULTRAsecreT')
     }
-    },
-  async login (parent, args, {prisma}, info){
-    const user = await prisma.query.user({
-      where: {email:args.data.email}
+  },
+  async login(parent, args, {prisma}, info) {
+    const user = await
+    prisma.query.user({
+      where: {email: args.data.email}
     });
     if (!user) throw new Error('Unable to login');
-    const match = await bcrypt.compare(args.data.password, user.password);
+    const match = await
+    bcrypt.compare(args.data.password, user.password);
     if (!match) throw new Error('Unable to login');
     return {
       user,
-      token: jwt.sign({userID:user.id}, 'ThisIsULTRAsecreT')
+      token: jwt.sign({userID: user.id}, 'ThisIsULTRAsecreT')
     }
   },
-  async updateUser(parent, args, {prisma}, info) {
-    const userExists = await
-    prisma.exists.User({id: args.id});
-    if (!userExists) throw new Error('No user found');
+  async updateUser(parent, args, {prisma, request}, info) {
+    const userID = getUserID(request);
     return prisma.mutation.updateUser({
-      where: {id: args.id},
+      where: {id: userID},
       data: args.data
     }, info);
   },
-  async deleteUser(parent, args, {prisma}, info) {
-    const userExists = await prisma.exists.User({id: args.id});
-    if (!userExists) throw new Error('No user found');
+  async deleteUser(parent, args, {prisma, request}, info) {
+    const userID = getUserID(request);
     return prisma.mutation.deleteUser({
-      where: {id: args.id}
+      where: {id: userID}
     }, info);
   },
   async createPost(parent, args, {prisma, request}, info) {
     const userID = getUserID(request);
-
     return prisma.mutation.createPost({
       data: {
         title: args.data.title,

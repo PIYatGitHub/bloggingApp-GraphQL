bfa4f580fb5bd25a04be2773575ea27d3ffb309c
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2019-07-24 19:21:23 +0300

    create comment is now locked down -- only 2 remaining...

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -82,12 +82,13 @@ const Mutation = {
       where: {id: args.id}
     }, info);
   },
-  async createComment(parent, args, {prisma}, info) {
+  async createComment(parent, args, {prisma, request}, info) {
+    const userID = getUserID(request);
     return prisma.mutation.createComment({
       data: {
         text: args.data.text,
         author: {
-          connect: {id: args.data.author}
+          connect: {id: userID}
         },
         post: {
           connect: {id: args.data.post}

301af1b8c9f96f01c81a81fba1dd8caa9f1c7f4f
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2019-07-26 08:34:02 +0300

    added a small refactor for the server startup code...

--- src/index.js
+++ src/index.js
@@ -1,17 +1,3 @@
 import '@babel/polyfill/noConflict'
-import {GraphQLServer} from 'graphql-yoga'
-import prisma from './prisma'
-import {resolvers, fragmentReplacements} from './resolvers/index'
-
-const server = new GraphQLServer({
-  typeDefs:'./src/schema.graphql',
-  resolvers,
-  fragmentReplacements,
-  context(request){
-    return{
-      prisma, request
-    }
-  }
-});
-
+import server from './sever'
 server.start({port:process.env.PORT || 4000}, ()=>console.log('the server is up on port 4000'));
